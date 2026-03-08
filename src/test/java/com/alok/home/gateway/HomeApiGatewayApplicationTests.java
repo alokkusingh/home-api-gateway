9ba97b234da0c714358e5ec353b8a35fd10b45be
alokkusingh <alok.ku.singh@gmail.com>
2026-03-08 12:04:55 +0530

    First commit

---
+++ src/test/java/com/alok/home/gateway/HomeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok.home.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
